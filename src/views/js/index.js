a37bc6b97a9b282b6c06a11c5fa2a75ab5d9e0a2
josemoralesmc <josemorales270102@gmail.com>
2021-07-09 21:18:20 -0300

    No se

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,27 +1,73 @@
 const input = document.getElementById('input');
 const CtnMsg = document.getElementById('Ctn-Msg');
 const IconMsg = document.getElementById('IconMsg');
+const GridCtn = document.getElementById('Grid-ctn');
+
+
+
+
+
+let array = ["Hola", "Como", "estas", "Cristian", "Jose", "Prueba", "Ojala que ande", "Me muero si no anda"]
+let length = array.length
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+let NumberY = Math.floor(Math.random() * (5 - 1)) + 1;
+let NumberX = Math.floor(Math.random() * (5 - 1)) + 1;
+console.log(NumberY);
+console.log(NumberX);
 
 // Funcion para enviar el mensaje
 
-input.onkeydown = function(e){
-    if(e.keyCode == 13){
-        input.value = "";
+let time = 3000
+
+
+
+input.onkeydown = function (e) {
+    if (e.keyCode == 13) {
+        let children = GridCtn.childNodes
+
+        array.push(input.value)
+        for (let i = 0; i <= (array.length - 1); i++) {
+            setTimeout(() => {
+                GridCtn.insertAdjacentHTML("beforeend", ` <div class="CtnM" style="grid-column: ${getRandomInt(1, 5)}; grid-row:${getRandomInt(1, 5)}" id="aa">
+            <div>${array[i]}</div> 
+        </div>` );
+       }, time);
+
+            time += 2000;
+
+
+
+
+
+        }
         CtnMsg.classList.add("Ctn-inputmsgSend");
         IconMsg.classList.toggle("Ctn-iconmsgSend");
         input.disabled = true;
     }
- };
 
- IconMsg.addEventListener("click", ClickImgMsg)
+}
+
+
+
+IconMsg.addEventListener("click", ClickImgMsg)
 
 //Funcion al apretar en el icono del mensaje para mandar otro
 
- function ClickImgMsg() {
-     CtnMsg.classList.replace("Ctn-inputmsgSend", "Ctn-inputmsg");
-     IconMsg.classList.replace("Ctn-iconmsgSend", "Ctn-iconmsg");
-     setTimeout(function(){ IconMsg.classList.remove("Ctn-iconmsg") }, 1700);
-     input.disabled = false;
-     console.log("gola");
-     
- }
+function ClickImgMsg() {
+    input.value = ""
+    input.disabled = false;
+    CtnMsg.classList.replace("Ctn-inputmsgSend", "Ctn-inputmsg");
+    IconMsg.classList.replace("Ctn-iconmsgSend", "Ctn-iconmsg");
+    Mensaje.classList.replace("CtnM", "CtnMEs")
+    setTimeout(function () { IconMsg.classList.remove("Ctn-iconmsg"); Mensaje.classList.remove("CtnMEs") }, 1700);
+    input.disabled = false;
+    console.log("gola");
+
+}
+
