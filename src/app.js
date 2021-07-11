eecb4af34ce1ebccee33e3c2e7130e4c5e6e611c
Cristian Cuello <cristiancuello10@gmail.com>
2021-07-11 20:00:09 -0300

    static, urlencoded, views

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 const phraseRoutes = require("./routes/phrase.routes")
-
+const path = require("path")
 const db = require("./db")
+
+//Dtabase
 db()
 
+//Views & static
+app.set("views", path.join(__dirname, "./views"))
+app.set(express.static(path.join(__dirname, "./static")))
+
+//Middlewares
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))//Recibe datos unicamente strings o arrays
 
 //Routes
 app.use("", phraseRoutes)
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
